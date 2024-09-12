2d47a50bb0a2c136b5773782f69fe17c9dd66555
Evan Nemerson <evan@nemerson.com>
2024-09-12 16:19:15 -0400

    ci: add Pelles-C compilation on GitHub Actions

--- .appveyor.bat
+++ .appveyor.bat
@@ -1,10 +1,5 @@
 cd test
 
-if "%COMPILER%"=="pelles" (
-  choco install pelles-c
-  "C:\Program Files\PellesC\Bin\povars64.bat"
-  pomake /F Makefile.pelles
-)
 if [%COMPILER%] == [msvc] (
   if []==[%VS_VERSION%] (
     "C:\Program Files (x86)\Microsoft Visual Studio\%VS_YEAR%\Community\VC\Auxiliary\Build\vcvarsall.bat" x86
